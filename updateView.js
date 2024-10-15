f6cf847ea44f04d92eba8ea762643719af0835bd
Geir Okkenhaug Jerstad <geir@geokkjer.eu>
2024-10-15 13:13:03 +0200

    added updateView

---
+++ updateView.js
@@ -0,0 +1,10 @@
+function updateView() {
+    let page = model.app.currentPage;
+    if (page == "home") {
+        viewHome();
+    } else if (page == "login") {
+        viewLogin();
+    } else {
+        document.getElementById('app').innerHTML = 'Error!!'
+    }
+}
--- viewHome.js
+++ viewHome.js
@@ -1,4 +1,5 @@
 function viewHome(){
+    header();
     document.getElementById('app').innerHTML = /*HTML*/`
         <h1>Steez Meet</h1>
     `
