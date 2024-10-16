cd504c2fc3fa7060c3d550f0d3ad61ac019e20bb
Geir Okkenhaug Jerstad <geir@geokkjer.eu>
2024-10-16 10:54:22 +0200

    added inputs and button to login page

--- viewLogin.js
+++ viewLogin.js
@@ -1,4 +1,8 @@
 function viewLogin(){
     header()
-    document.getElementById('app').innerHTML += '<h1>Hello from Loginpage</h1>'
+    document.getElementById('app').innerHTML += /*HTML*/ `<h1>Hello from Loginpage</h1>
+    Brukernavn: <input type="text" id="username"><br>
+    Passord: <input type="password" id=""><br>
+    <button>Logg inn</button>
+    `;
 }
