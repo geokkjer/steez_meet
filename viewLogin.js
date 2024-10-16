d17da68d4a2543f3cbbd8c89e8c988438a1bc38c
Geir Okkenhaug Jerstad <geir@geokkjer.eu>
2024-10-16 12:19:52 +0200

    small change viewlogin

--- viewLogin.js
+++ viewLogin.js
@@ -3,9 +3,10 @@ function viewLogin(){
     let username = '';
     let password = '';
     document.getElementById('app').innerHTML += /*HTML*/ `<h1>Hello from Loginpage</h1>
-    Brukernavn: <input type="text" id="username" onchange="username=this.value"><br>
+    Brukernavn: <input type="text" id="username" placeholder="Brukernavn" onchange="username=this.value"><br>
     Passord: <input type="password" id="password" onchange="password=this.value"><br>
-    <button onclick="login(username,password)">Logg inn</button>
+    <button onclick="login(username,password)">Logg inn</button><br>
+    <div>husk meg! glemt passord ? registrere ny bruker </div>
     `;
 }
 
