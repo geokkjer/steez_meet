1d95e819f2891172204ee7570352d81e728fa7da
Geir Okkenhaug Jerstad <geir@geokkjer.eu>
2024-10-16 14:41:49 +0200

    started list of arrangements on main page

--- viewHome.js
+++ viewHome.js
@@ -1,6 +1,23 @@
 function viewHome(){
     header();
     document.getElementById('app').innerHTML += /*HTML*/`
-        <h1>Steez Meet</h1>
+        <h1>Arrangementer:</h1>
     `
+    events();
+}
+
+function events(){
+    let event = '';
+    let comment = '';
+    let users = [];
+    for (let i = 0; i < model.data.events.length; i ++){
+        users.push(model.data.users[i].firstName);
+        event = model.data.events[i];
+        
+        // comment = model.data.events[i].comment[i];
+        document.getElementById('app').innerHTML += /*HTML*/ `
+            <li>Navn: ${event.name} Når: ${event.date} Kommenterer: ${event.comment[0].comment} Deltakere: ${users}</li>
+        `
+    }
+    console.log(users)
 }
