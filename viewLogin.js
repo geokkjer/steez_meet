79b8478693f2508e4a0b92764f1960ee7df7c13b
Geir Okkenhaug Jerstad <geir@geokkjer.eu>
2024-10-16 13:06:58 +0200

    some functionality to login page

--- viewLogin.js
+++ viewLogin.js
@@ -4,7 +4,7 @@ function viewLogin(){
     let password = '';
     document.getElementById('app').innerHTML += /*HTML*/ `<h1>Hello from Loginpage</h1>
     Brukernavn: <input type="text" id="username" placeholder="Brukernavn" onchange="username=this.value"><br>
-    Passord: <input type="password" id="password" onchange="password=this.value"><br>
+    Passord: <input type="password" id="password" placeholder="Passord" onchange="password=this.value"><br>
     <button onclick="login(username,password)">Logg inn</button><br>
     <div>husk meg! glemt passord ? registrere ny bruker </div>
     `;
@@ -13,8 +13,9 @@ function viewLogin(){
 function login(user,pass){
     console.log(user,pass);
     for (modeluser of model.data.users){
-        if (modeluser == user){
-            console.log('Hello')
+        console.log(modeluser.firstName, modeluser.password)
+        if (modeluser.userName == user && modeluser.password == pass){
+            console.log('Hello', modeluser.firstName, model.password)
         }
     }
     // if (user = model.data.users){
