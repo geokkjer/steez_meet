52b168dfc623a859e3c197bf0d7fa7cfa4521a7a
DanielLarsen-spec <larsen97daniel@gmail.com>
2024-10-16 11:08:21 +0200

    Update viewNewEvent.js

---
+++ viewNewEvent.js
@@ -0,0 +1,32 @@
+
+function viewNewEvent(){
+    header()
+    document.getElementById('app').innerHTML += /*html*/`
+    
+    <h2>Nytt arrangement:</h2>
+    <div>navn:</div>
+    <input type="text" onchange="nameArrangement()">
+
+    <div>Dato:</div>
+    <input type="text" onchange="datesArrangement()">
+    <div>Bakke:</div>
+    <input type="text" onchange="datesArrangement()">
+    <div>Kommentar:</div>
+    <input type="text" onchange="commentsArrangement()">
+    
+    
+    
+    `;
+    
+    
+}
+
+
+
+
+
+
+
+function eventAnswer(){
+    
+}
