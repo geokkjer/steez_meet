f6a66d1fb03e0b381993a1ca6bbc708867bfd8c0
xStarRier <stephanniequynh@gmail.com>
2024-10-16 13:39:05 +0200

    Update viewSlopeInfo.js

--- viewSlopeInfo.js
+++ viewSlopeInfo.js
@@ -2,8 +2,8 @@ function viewSlopeInfo(){
     header();
     document.getElementById('app').innerHTML += /*HTML*/` 
     <h1>Bakke Info:</h1>
-    <h2>Rangering<h2>
-    <div>0/5</div>
+    <h2>Rangering:<h2>
+    <div>1/5</div>
     <input id=textInput type=text/>
     `;
    }
