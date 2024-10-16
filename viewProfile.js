30217971945aff5897436d1a51b2eca1f9b9d6f2
OriginalKilljoy <162512500+OriginalKilljoy@users.noreply.github.com>
2024-10-16 11:19:59 +0200

    Begun profile with goals

--- viewProfile.js
+++ viewProfile.js
@@ -1,4 +1,36 @@
 function viewProfile() {
     header();
-    document.getElementById('app').innerHTML += '<h1>Hello from profile</h1>';
+    document.getElementById('app').innerHTML += /*HTML */ `
+    <img src= "${model.data.users}">
+    <div>Personalia:</div>
+    <div>Events:</div>
+    <div>Goals:</div>
+    <button></button>
+    `
+    ; 
 }
+
+function createGoal(){
+    header();
+    document.getElementById('app').innerHTML += /*HTML */ `
+    <img src= "${model.data.users}">
+    <div>Personalia:</div>
+    <div>Events:</div>
+    <div>Goals:</div>
+    <div>Ditt mål:</div>
+    <input type="text">
+    <div>Beskrivelse:</div>
+    <input type="text">
+    <div>Antall utførelser:</div>
+    <input type="number">
+    `
+    ; 
+}
+
+//Vi trenger
+
+//Inputfelt - tittel
+//Inputfelt - beskrivelse
+//Mulighet for å sette antall
+//Muligfhet for å måle progresjon
+//Mulighet for å checke av når man har utført en session
