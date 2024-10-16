ec31eeecc232ccd70341f316508cb70c7a22bc20
xStarRier <stephanniequynh@gmail.com>
2024-10-16 14:56:45 +0200

    Update viewSlopeInfo.js

--- viewSlopeInfo.js
+++ viewSlopeInfo.js
@@ -2,14 +2,14 @@
 
 
 function viewSlopeInfo(){
-   let rating = 0; 
-   header();
-   document.getElementById('app').innerHTML += /*HTML*/` 
-   <h1>Bakke Informasjon:</h1>
-   <h2>Rangering:<h2>
-   <div id="rateMe"></div>
-   <input id='textInput' type='number' min=0 max= 5 onchange="rating=this.value" placeholder="0/5"/>
-   <button onclick="ratings(rating)">Legg til rangering</button>
+    let rating = 0; 
+    header();
+    document.getElementById('app').innerHTML += /*HTML*/` 
+     <h1>Bakke Informasjon:</h1>
+     <h2>Rangering:<h2>
+     <div id="rateMe"></div>
+     <input id='textInput' type='number' min=0 max= 5 onchange="rating=this.value" placeholder="0/5"/>
+     <button onclick="ratings(rating)">Legg til rangering</button>
    
     <h2>Legg til kommentar</h2>
     <textarea rows="8" cols="35" type="text" id="inputCommentary" placeholder="Skriv din kommentar her..."></textarea>
@@ -21,6 +21,7 @@ function viewSlopeInfo(){
 function ratings(rating){
     document.getElementById('rateMe').innerHTML += `<h2>${rating}/5<h2>`
 }
+
 
         document.getElementById('submitCommentary').addEventListener('click', function() {
             const textInput = document.getElementById('inputCommentary');
