94ea3a2d2f2e1828e7f3ec28a88d5978906783e9
lars1349 <mostuelars@gmail.com>
2024-10-16 14:22:46 +0200

    Update viewSlopeInfo.js

--- viewSlopeInfo.js
+++ viewSlopeInfo.js
@@ -10,8 +10,24 @@ function viewSlopeInfo(){
    <div id="rateMe"></div>
    <input id='textInput' type='number' min=0 max= 5 onchange="rating=this.value"/>
    <button onclick="ratings(rating)">Legg til rating</button>
+   
+    <h2>Legg til kommentar</h2>
+    <textarea rows="8" cols="35" type="text" id="inputCommentary" placeholder="Skriv din kommentar her..."></textarea>
+    <br><button id="submitCommentary">Legg til kommentar!</button><p>
+    <h3>Kommentarer til bakken:</h3>
+    <ul id="addCommentary"></ul>
    `}
 
 function ratings(rating){
     document.getElementById('rateMe').innerHTML += `<h2>${rating}/5<h2>`
 }
+
+        document.getElementById('submitCommentary').addEventListener('click', function() {
+            const textInput = document.getElementById('inputCommentary');
+            const output = document.getElementById('addCommentary');
+            const inputText = textInput.value;
+            if (inputText !== "") {
+                output.innerHTML += `<li>${inputText}</li>`;
+                textInput.value = "";
+            }
+        });
