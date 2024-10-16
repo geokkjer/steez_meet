66b4b054c87842c78dad609a1722ef0b113d2155
OriginalKilljoy <162512500+OriginalKilljoy@users.noreply.github.com>
2024-10-16 12:25:34 +0200

    Updates to profile

--- viewProfile.js
+++ viewProfile.js
@@ -1,11 +1,15 @@
 function viewProfile() {
     header();
     document.getElementById('app').innerHTML += /*HTML */ `
-    <img src= "${model.data.users}">
+    <img src= "${model.data.users[0].profilePicture}">
     <div>Personalia:</div>
-    <div>Events:</div>
-    <div>Goals:</div>
-    <button></button>
+    <div>Navn: ${model.data.users[0].firstName} ${model.data.users[0].lastName}</div>
+    <div>Epost: ${model.data.users[0].email}</div>
+    <div>Adresse: ${model.data.users[0].address}</div>
+    <div>Mobil: ${model.data.users[0].phoneNumber}</div>
+    <div>Arrangementer:</div>
+    <div>Mål:</div>
+    <button onclick="createGoal()">+</button>
     `
     ; 
 }
@@ -13,12 +17,12 @@ function viewProfile() {
 function createGoal(){
     header();
     document.getElementById('app').innerHTML += /*HTML */ `
-    <img src= "${model.data.users}">
+    <img src= "${model.data.users[0].profilePicture}">
     <div>Personalia:</div>
     <div>Events:</div>
     <div>Goals:</div>
     <div>Ditt mål:</div>
-    <input type="text">
+    <input type="text" placeholder="Skriv inn ditt mål">
     <div>Beskrivelse:</div>
     <input type="text">
     <div>Antall utførelser:</div>
