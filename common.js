5d7467509dc8dac12628a27df200b7c5488e9367
Geir Okkenhaug Jerstad <geir@geokkjer.eu>
2024-10-16 09:35:47 +0200

    added logo to headerbar

--- common.js
+++ common.js
@@ -1,9 +1,10 @@
 function header() {
     document.getElementById('app').innerHTML = /*HTML*/`
     <link rel="stylesheet" href="header.css">
-    <div class="topnav" id="myTopnav">
-        <a href="#home" onclick="updateView('home')">🏠 Hjem</a>
-        <a href="#profile" onclick="updateView('profile')">🛈 Din Profil</a>
+    <div class="topnav" id="header">
+        <img class="logo" src="img/logo.png"/>
+        <a href="#home" onclick="model.app.currentPage = 'home';updateView()">🏠 Hjem</a>
+        <a href="#profile" onclick="updateView()">🛈 Din Profil</a>
         <a href="#slopes">⛰ Bakker</a>
         <a href="#" id="login" onclick="model.app.currentPage = 'login';updateView()">⎆ Login</a>
    </div>`
