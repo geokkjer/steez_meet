427e86f843983467cbc190e30645e9e912dbff96
DanielLarsen-spec <larsen97daniel@gmail.com>
2024-10-16 14:53:30 +0200

    Update viewNewEvent.js

--- viewNewEvent.js
+++ viewNewEvent.js
@@ -1,35 +1,39 @@
-
+let eventText ="";
 function viewNewEvent(){
     header()
+    let nameArrangement
+
     document.getElementById('app').innerHTML += /*html*/`
     
     <h2>Nytt arrangement:</h2>
     <div>navn:</div>
-    <input type="text" onchange="nameArrangement(this.value)">
+    <input type="text" onchange="nameArrangement = this.value">
 
     <div>Dato:</div>
-    <input type="date" onchange="datesArrangement(this.value)">
+    <input type="date" onchange="datesArrangement = this.value">
     <div>Bakke:</div>
-    <input type="search" onchange="slopeArrangement(this.value)">
+    <input type="text" onchange="slopeArrangement = this.value">
     <div>Kommentar:</div>
-    <textarea type="text" onchange="commentsArrangement(this.value)"></textarea>
+    <textarea type="text" onchange="commentsArrangement = this.value"></textarea>
     
-    <button onclick="Submit()">Submit</button>
+    <button onclick="Submit(text)">Submit</button>
+    
+    <div>${eventText}</div>
+    
     `;
-    
-    
-    
     
 }
 
-function Submit(){
+function Submit(name, dates, slope, comments){
+    
+    eventText += name += dates += slope += comments
+    return eventText;
+
+
+
     
 
 
-
-
-
-    
 }
 
 
