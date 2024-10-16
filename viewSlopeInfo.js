f3fa77f241ad6493ee5bcf295ab73db9c96c56fe
xStarRier <stephanniequynh@gmail.com>
2024-10-16 14:33:19 +0200

    Update viewSlopeInfo.js

--- viewSlopeInfo.js
+++ viewSlopeInfo.js
@@ -9,7 +9,7 @@ function viewSlopeInfo(){
    <h2>Rangering:<h2>
    <div id="rateMe"></div>
    <input id='textInput' type='number' min=0 max= 5 onchange="rating=this.value" placeholder="0/5"/>
-   <button onclick="ratings(rating)">Legg til rating</button>
+   <button onclick="ratings(rating)">Legg til rangering</button>
    
     <h2>Legg til kommentar</h2>
     <textarea rows="8" cols="35" type="text" id="inputCommentary" placeholder="Skriv din kommentar her..."></textarea>
