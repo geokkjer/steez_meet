d7edbf1676ceea4444a70fd8027cfd7b7d58613d
xStarRier <stephanniequynh@gmail.com>
2024-10-21 14:14:09 +0200

    oimnom

--- slopeinfo/viewSlopeInfo.js
+++ slopeinfo/viewSlopeInfo.js
@@ -2,7 +2,8 @@
 
 
 function viewSlopeInfo(){
-    let rating = 0; 
+    
+    let rating = model.date.slope.rating; 
     header();
     document.getElementById('app').innerHTML += /*HTML*/` 
      <h1>Bakke Informasjon:</h1>
@@ -10,20 +11,32 @@ function viewSlopeInfo(){
      <div id="rateMe"></div>
      <input id='textInput' type='number' min=0 max= 5 onchange="rating=this.value" placeholder="0/5"/>
      <button onclick="ratings(rating)">Legg til rangering</button>
-     <h1>Rangeringer</h1>
+    <br>
+    <div id="avgRating"></div
+    <br>
+    <h3>Rangeringer til bakken:</h3>
+    
+    <button onclick="ratingAvg(ratings)">Rangeringer på bakken</button>
+     <div id="avgRating">5/5</div
+    <br> 
     <h2>Legg til kommentar</h2>
     <textarea rows="8" cols="35" type="text" id="inputCommentary" placeholder="Skriv din kommentar her..."></textarea>
     <br><button id="submitCommentary">Legg til kommentar!</button><p>
-    <h3>Kommentarer til bakken:</h3>
     <ul id="addCommentary"></ul>
+    <h3>Kommentarer til bakken:</h3>
+ 
+    
    `}
 
 function ratings(rating){
     document.getElementById('rateMe').innerHTML += `<h2>${rating}/5<h2>`
 }
-function ratingData(){
-        
+function ratingData(ratings){
+    document.getElementById('rateMe').innerHTML += `<h2>${ratings}/5<h2>` 
+    
+
 }
+
         document.getElementById('submitCommentary').addEventListener('click', function() {
             const textInput = document.getElementById('inputCommentary');
             const output = document.getElementById('addCommentary');
