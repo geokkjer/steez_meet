32179dcdd27909b7e3b01bfdff9b2932febb2046
Geir Okkenhaug Jerstad <geir@geokkjer.eu>
2024-10-23 14:14:15 +0200

    gradient

--- header/header.js
+++ header/header.js
@@ -6,9 +6,9 @@ function header() {
         <ul>
         <li><img class="logo" src="img/logo.png"/></a></li>
         <li class="active" onmousedown="viewHome()">Hjem </li>
-        <li onclick="viewProfile()">| Din Profil </li>
+        <li onclick="viewProfile()">|Din Profil </li>
         <li id="login" onmousedown="viewLogin()">⎆ Login</li>
-        <li onmousedown="viewNewEvent()">| Legg til Event</li>
+        <li onmousedown="viewNewEvent()">|Legg til Event</li>
         <select id="bakker" onchange="viewSlopeInfo()">
                 <option> ⛰Velg Bakke </option>
                 <option onclick="viewSlopeInfo()">Bakke 1</option>
