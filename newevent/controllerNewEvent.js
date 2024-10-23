c77979a93446b06cbec4ff0137e38eb2a812733c
DanielLarsen-spec <larsen97daniel@gmail.com>
2024-10-23 14:34:56 +0200

    update

--- newevent/controllerNewEvent.js
+++ newevent/controllerNewEvent.js
@@ -1,32 +1,16 @@
-function saveName(input){
-model.input.newEvent.name = input;
-}
-function saveDate(input){
-model.input.newEvent.date = input;
-}
-function saveSlope(input){
-model.input.newEvent.slope = input;
-}
-function saveComment(input){
-model.input.newEvent.comment = input;
-}
+let newEvent = model.input.newEvent;
+function addEvent() {
+  let myEvent = {
+    name: newEvent.name,
+    date: newEvent.date,
+    slope: newEvent.slope,
+    comment: newEvent.comment,
+  };
+  model.data.events.push(myEvent);
+  newEvent.name = "";
+  newEvent.date = "";
+  newEvent.slope = "";
+  newEvent.comment = "";
 
-function addEvent(){
-    const newEvent = {
-        name: model.input.newEvent.name,
-        date: model.input.newEvent.date,
-        slope: model.input.newEvent.slope,
-        comment: model.input.newEvent.comment,
-    }
-    model.data.events.push(newEvent)
-    emptyNewEventValues();
-    viewNewEvent()    
-}
-
-function emptyNewEventValues(){
-    model.input.newEvent.name = null;
-    model.input.newEvent.date = null;
-    model.input.newEvent.slope = null;
-    model.input.newEvent.comment = null;
-
+  viewNewEvent();
 }
--- newevent/viewNewEvent.js
+++ newevent/viewNewEvent.js
@@ -1,21 +1,21 @@
-viewNewEvent()
-function viewNewEvent(){
-    header()
-    let html = /*html*/`
-    <h3>Nytt arrangement: </h3>
+viewNewEvent();
+function viewNewEvent() {
+  header();
+  let html = /*html*/ `
+    <h2>Nytt arrangement: </h2>
     <div>${showAddEvent()}</div>
     <div>${showEvent()}</div>
 
 
     
     `;
-    document.getElementById('main').innerHTML =  html;   
+  document.getElementById("main").innerHTML = html;
 }
 
-function showEvent(){
-let html = "";
-for(let i = 0; i < model.data.events.length; i++){
-    html += /*html*/`
+function showEvent() {
+  let html = "";
+  for (let i = 0; i < model.data.events.length; i++) {
+    html += /*html*/ `
     <p>Navn: ${model.data.events[i].name}</p>
     <p>Dato: ${model.data.events[i].date}</p>
     <p>Bakke: ${model.data.events[i].slope}</p>
@@ -23,58 +23,41 @@ for(let i = 0; i < model.data.events.length; i++){
    
     
     `;
-}
-    
-    return html;
-    
-    
-}    
+  }
 
-function showAddEvent(){
-let html = "";
-html += /*html*/`
+  return html;
+}
+
+function showAddEvent() {
+  let html = "";
+  html += /*html*/ `
 <h3>Legg til arrangement</h3>
 <div>
     <label>Navn:</label>
-    <input type="text" onchange="saveName(this.value)">
+    <input type="text" onchange="model.input.newEvent.name = this.value">
 
 </div>
 <div>
     <label>Dato:</label>
-    <input type="date" onchange="saveDate(this.value)">
+    <input type="date" onchange="model.input.newEvent.date = this.value">
 
 </div>
 <div>
     <label>bakke:</label>
-    <input type="text" onchange="saveSlope(this.value)">
+    <input type="text" onchange="model.input.newEvent.slope = this.value">
 
 </div>
 <div>
     <label>Kommentar:</label>
-    <input type="text" onchange="saveComments(this.value)">
+    <input type="text" onchange= "model.input.newEvent.comment = this.value">
 
 </div>
 <button onclick="addEvent()">Legg til</button>
 
 
 `;
-return html;
+  return html;
 }
-    
-
-
-
-
-    
-    
-    
-    
-    
-
-
-
-
-
 
 //Vi trenger: Visning av arrangementer.
 //Ja/Nei funksjon.
