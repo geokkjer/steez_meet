dc43d748945a2d282f4cfef767dbe227b40e33a4
Geir Okkenhaug Jerstad <geir@geokkjer.eu>
2024-10-23 13:36:09 +0200

    header fix

--- home/controlHome.js
+++ home/controlHome.js
@@ -1,14 +1,14 @@
 function events(){
     
     let users = [];
-    let html = '';
+    let eventsHtml = '';
     for (let i = 0; i < model.data.events.length; i ++){
         users.push(model.data.users[i].firstName);
     }
     for (let event of model.data.events){
-        html += /*html*/ `
+        eventsHtml += /*html*/ `
             <li>Navn: ${event.name} Når: ${event.date} Kommenterer: ${users}</li>
         `
     }
-    return html
+    return eventsHtml
 }
