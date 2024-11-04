a258d90cc0b6d830c9092be584eaa7bedc0e1f09
Geir Okkenhaug Jerstad <geir@geokkjer.eu>
2024-11-04 10:21:52 +0100

    TODO og litt på login

--- login/viewLogin.js
+++ login/viewLogin.js
@@ -34,6 +34,7 @@ function login(user,pass){
     for (modeluser of model.data.users){
         if (modeluser.userName == user && modeluser.password == pass){
             model.app.currentPage = "home";
+            model.app.loggedInId = model.data.users /* finn brukernavn */
             updateView();
         } else if (modeluser.userName != user || modeluser.password != pass){
             console.log('Feil brukernavn eller passord :-P')
