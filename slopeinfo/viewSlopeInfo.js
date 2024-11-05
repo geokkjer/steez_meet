ae7b030832895cd0ccc87bf4b1e8c5521e9e2366
xStarRier <stephanniequynh@gmail.com>
2024-11-05 15:04:50 +0100

    dwdw

--- slopeinfo/viewSlopeInfo.js
+++ slopeinfo/viewSlopeInfo.js
@@ -4,42 +4,41 @@ let avgRatings = model.data.slopes[0].avgRating;
 function viewSlopeInfo() {
 let slopeHTML = /*HTML*/` 
 <h1>Bakke Informasjon:</h1>
-<div id="gridInfo">
-<h1>Bakkenavn: Hafjell</h1>
-<h1>Vanskelighetsgrad: 3/5</h1>
-<h1>Type bakke:</h1>
-
-</div>
+<div id="gridSlopeSite">
 <div class="slopePhoto">
 <img src="/img/slopeBlefjell.png" width="250px">
 </div>
+<div>
+<div class="gridInfo">
+<h1>Bakkenavn: Hafjell</h1>
+<h1>Vanskelighetsgrad: 3/5</h1>
+<h1>Type bakke:</h1>
 </div>
-
-<div id="GridSlopeInfo">
-
+<div id="ridSlopeInfo">
 <div class="publicInfo">
 <h1>Bakkerangering:</h1>
 <div id="showAvgRating"></div>
 <h2>Kommentarer til bakken:</h2>
 <ul id="commentList"></ul>
-</div>
-
 <div class="mySlope">
  <h1>Din rangering:<h1>
      <div id="rateMe"></div>
      <input id='textInput' type='number' min=1 max= 5 onchange="rating=this.value" placeholder="0/5"/>
      <button onclick="ratings(rating),ratingData()">Legg til rangering</button>
-     
+
 <h1>Legg til kommentar:</h1>
-    <textarea rows="8" cols="35" id="submitInput" placeholder="Skriv din kommentar her..."></textarea><p>
-   <button onclick="addComment()">Send inn kommentar:</button>
-    <button onclick="viewAddSlope()">Legg til bakke</button>
+<textarea rows="8" cols="35" id="submitInput" placeholder="Skriv din kommentar her..."></textarea><p>
+<button onclick="addComment()">Send inn kommentar:</button>
+<button onclick="viewAddSlope()">Legg til bakke</button>
+</div>
+</div>
+</div>
+
+</div>
 </div>
 
 
-
 </div>
-
 
 
 
