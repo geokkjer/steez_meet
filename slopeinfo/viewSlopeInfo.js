d9e91e1c6252067d1b53520b3d52af50946cc167
xStarRier <stephanniequynh@gmail.com>
2024-11-06 16:17:43 +0100

    final css for bakke

--- slopeinfo/viewSlopeInfo.js
+++ slopeinfo/viewSlopeInfo.js
@@ -28,12 +28,13 @@ function viewSlopeInfo(slopeId) {
 <div class="slopeInfo-mainContainer">
 <div id="gridSlopeSite">
     <div class="slopePhoto">
-    <img src="${slopeImage}" width="250px">
+    <img src="${slopeImage}" >
+    <button onclick="viewAddSlope()">Legg til bakke</button>
     </div>
 <div>
 <div class="gridInfo">
     <h1>Bakkenavn: ${slopeName}</h1>
-    <h2>Vanskelighetsgrad: 3/5</h2>
+    <h1>Vanskelighetsgrad: 3/5</h1>
     <h1>Beskrivelse:${slopeDescription}</h1>
 </div>
 <div id="gridSlopeInfo">
@@ -44,20 +45,24 @@ function viewSlopeInfo(slopeId) {
     <ul id="commentList">${slopeComments}</ul>
 </div>
 <div id="gridSlopeInfo">
-  <div class="mySlope">
-     <h1>Din rangering:<h1>
+<div id="myRating">
+<h1>Din rangering:<h1>
 <div id="rateMe">${slopeRatings}</div>
      <input id='textInput' type='number' min=1 max= 5 onchange="rating=this.value" placeholder="0/5"/>
      <button onclick="ratings(rating),ratingData()">Legg til rangering</button>
-     <h1>Legg til kommentar:</h1>
+
+<div class="myComment">
+     <h3>Legg til kommentar:</h3>
 <textarea rows="8" cols="35" id="submitInput" placeholder="Skriv din kommentar her..."></textarea><p>
+
+</div>
 <button onclick="addComment()">Send inn kommentar:</button>
+</div> 
 </div>
 </div>
 </div>
 </div>
-</div>
-<button onclick="viewAddSlope()">Legg til bakke</button>
+
    `;
   document.getElementById("main").innerHTML = slopeHTML;
 }
