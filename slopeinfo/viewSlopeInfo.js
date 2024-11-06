913bf3f80f18d08ec3b03053e789d868a8209c4d
xStarRier <stephanniequynh@gmail.com>
2024-11-06 13:00:26 +0100

    dwd
    wdwd

--- slopeinfo/viewSlopeInfo.js
+++ slopeinfo/viewSlopeInfo.js
@@ -26,45 +26,37 @@ function viewSlopeInfo(slopeId) {
   let slopeHTML = /*HTML*/ ` 
 <h1>Bakke Informasjon:</h1>
 <div id="gridSlopeSite">
-<div class="slopePhoto">
-<img src="${slopeImage}" width="250px">
+    <div class="slopePhoto">
+    <img src="${slopeImage}" width="250px">
 </div>
 <div>
 <div class="gridInfo">
-<h1>Bakkenavn: ${slopeName}</h1>
-<h1>Vanskelighetsgrad: 3/5</h1>
-<h1>Type bakke:${slopeDescription}</h1>
+    <h1>Bakkenavn: ${slopeName}</h1>
+    <h2>Vanskelighetsgrad: 3/5</h2>
+    <h1>Beskrivelse:${slopeDescription}</h1>
 </div>
-<div id="ridSlopeInfo">
-<div class="publicInfo">
-<h1>Bakkerangering:</h1>
+<div id="gridSlopeInfo">
+ <div class="publicInfo">
+    <h1>Bakkerangering:</h1>
 <div id="showAvgRating">${slopeAvgRating}</div>
-<h2>Kommentarer til bakken:</h2>
-<ul id="commentList">
-${slopeComments.map(comment => `<li>${comment.comment}</li>`).join('')}
-</ul>
-<div class="mySlope">
- <h1>Din rangering:<h1>
-     <div id="rateMe">${slopeRatings}</div>
+    <h2>Kommentarer til bakken:</h2>
+    <ul id="commentList">${slopeComments}</ul>
+</div>
+<div id="gridSlopeInfo">
+  <div class="mySlope">
+     <h1>Din rangering:<h1>
+<div id="rateMe">${slopeRatings}</div>
      <input id='textInput' type='number' min=1 max= 5 onchange="rating=this.value" placeholder="0/5"/>
      <button onclick="ratings(rating),ratingData()">Legg til rangering</button>
-
-<h1>Legg til kommentar:</h1>
+     <h1>Legg til kommentar:</h1>
 <textarea rows="8" cols="35" id="submitInput" placeholder="Skriv din kommentar her..."></textarea><p>
 <button onclick="addComment()">Send inn kommentar:</button>
+</div>
+</div>
+</div>
+</div>
+</div>
 <button onclick="viewAddSlope()">Legg til bakke</button>
-</div>
-</div>
-</div>
-
-</div>
-</div>
-
-
-</div>
-
-
-
    `;
   header();
   footer();
