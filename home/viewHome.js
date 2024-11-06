e62ec1d603d54e7e81e9fe3fd4fe7cabb583a03b
DanielLarsen-spec <larsen97daniel@gmail.com>
2024-11-06 09:42:55 +0100

    Update viewHome.js

--- home/viewHome.js
+++ home/viewHome.js
@@ -9,17 +9,16 @@ function drawHome() {
 function drawGreeting() {
     return htmlHome = /*HTML*/`
         <h1>Hei, ${'hello' || 'guest'}</h1>
-        <h2>Kommende turer: </h2>
         `;
 }
 function drawActivities() {
     let activities = model.data.events;
     return activityHTML = activities.map(activity => /*HTML*/`
-        
+        <h2>Kommende turer: </h2>
         <div id="grid">
             <div class="column1">
                 <h4>${activity.name}</h4>
-                <img src="${getSlopePhotoById(activity.id)}">
+                <img src="${model.data.slopes[0].photo}">
                 </div>
             <div class="column2">
                 <p><span>Når: </span>${activity.date}</p>
@@ -27,9 +26,9 @@ function drawActivities() {
             </div>
             <div class="column3">
                 <h2>Deltakere</h2>
-                <p id="part"> ${getParticipantsOfEvent(activity.members)}</p>
+                <p id="part"> ${getParticipantsOfEvent()}</p>
                 <h4>Bli med ?</h4>
-                <button value="ja" onclick="joinAnswer(this.value) model.app.currentEventId=activity.id" class="yes-noBtn">Ja</button>
+                <button value="ja" onclick="joinAnswer(this.value)" class="yes-noBtn">Ja</button>
                 <button value="nei" onclick="joinAnswer(this.value)" class="yes-noBtn">Nei</button>
             </div>
         </div>`
