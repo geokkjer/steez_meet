f6287b588547d5d73894f2de3ef3505ff43c90f9
KristianHeggdal <kristianheggdal@hotmail.no>
2024-11-07 10:13:10 +0100

    sdsdsds

--- slopeinfo/viewSlopeInfo.js
+++ slopeinfo/viewSlopeInfo.js
@@ -42,7 +42,7 @@ function viewSlopeInfo(slopeId) {
     <h1>Bakkerangering:</h1>
 <div id="showAvgRating">${slopeAvgRating}</div>
     <h2>Kommentarer til bakken:</h2>
-    <ul id="commentList">${slopeComments}</ul>
+    <ul id="commentList">${slopeComments.map(comment => `<li>${comment.comment}</li>`).join('')}</ul>
 </div>
 <div id="gridSlopeInfo">
 <div id="myRating">
