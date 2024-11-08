65a5be32601bc13801719118ac035989ee88002b
OriginalKilljoy <162512500+OriginalKilljoy@users.noreply.github.com>
2024-11-08 09:58:33 +0100

    Add slope styling

--- addslope/viewAddSlope.js
+++ addslope/viewAddSlope.js
@@ -1,16 +1,38 @@
 function viewAddSlope(){
 
     addSlopeHtml = /*HTML*/`
-    <div class="mainView">
-    <h2>Legg til bakke:<h2>
-    <div>Navn: <input type="text" onchange="model.input.addSlope.name = this.value"></div>
-    <div>Sted:<input type="text" onchange="model.input.addSlope.location = this.value"></div>
-    <div>Vanskelighetsgrad<input type="text" onchange="model.input.addSlope.difficulty = this.value"></div>
-    <div>Beskrivelse:<input type="text" onchange="model.input.addSlope.description = this.value"></div>
-    <div>Type: <input type="text" onchange="model.input.addSlope.type = this.value"></div>
-    <div>Kommentar <input type="text" onchange="model.input.addSlope.comment = this.value"></div>
-    <div>Bilde: <input type="file" accept="image/*" onchange="handleFileUpload(event)"></div>
-    <button onclick="submitSlope()">Submit</button>
+    <div class="addSlope-outsideContainer">
+        <div class="addSlope-container">
+            <div class="addSlope-title">
+                <h2>Legg til bakke:<h2>
+            </div>
+            <div class="addSlope-inputContainer"> 
+                <div class="addSlope-name">Navn:</div>
+                <input class="addSlope-inputName" type="text" onchange="model.input.addSlope.name = this.value">
+                
+                <div class="addSlope-place">Sted:</div>
+                <input class="addSlope-inputPlace" type="text" onchange="model.input.addSlope.location = this.value">
+                
+                <div class="addSlope-difficulty">Vanskelighetsgrad</div>
+                <input class="addSlope-inputDifficulty" type="text" onchange="model.input.addSlope.difficulty = this.value">
+                
+                <div class="addSlope-description">Beskrivelse:</div>
+                <input class="addSlope-inputDescription" type="text" onchange="model.input.addSlope.description = this.value">
+                
+                <div class="addSlope-type">Type:</div>
+                <input class="addSlope-inputType" type="text" onchange="model.input.addSlope.type = this.value">
+                
+                <div class="addSlope-comment">Kommentar:</div>
+                <input class="addSlope-inputComment" type="text" onchange="model.input.addSlope.comment = this.value">
+                
+                <div class="addSlope-image">Bilde:</div>
+                <input class="addSlope-inputImage" type="file" accept="image/*" onchange="handleFileUpload(event)"></div>
+                
+                <div class="addSlope-addButtonContainer">
+                    <button onclick="submitSlope()">Legg til</button>
+                </div>
+            </div>
+        </div>
     </div>
     `
     document.getElementById('main').innerHTML =  addSlopeHtml;
